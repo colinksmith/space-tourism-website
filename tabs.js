326141c6bac21c31214d8dede1437dc4868c40d6
Colin Smith <88501518+colinksmith@users.noreply.github.com>
2023-01-04 16:34:09 -0500

    refactoring js for readability

--- tabs.js
+++ tabs.js
@@ -8,46 +8,59 @@ tabs.forEach((tab) => {
     tab.addEventListener('click', changeTabPanel)
 })
 
-let tabFocus = 0
+let tabFocus = 0;
 function changeTabFocus(e) {
-    const keydownLeft = 37
-    const keydownRight = 39
-
+    const keydownLeft = 37;
+    const keydownRight = 39;
+    
     if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
-        tabs[tabFocus].setAttribute('tabindex', -1)
-    }
-
-    if (e.keyCode === keydownRight) {
-        tabFocus++
-        if (tabFocus >= tabs.length) {
-            tabFocus = 0
+        tabs[tabFocus].setAttribute("tabindex", -1);
+        
+        if (e.keyCode === keydownRight) {
+            tabFocus++;
+            if (tabFocus >= tabs.length) {
+                tabFocus = 0;
+            }
+        } else if (e.keyCode === keydownLeft) {
+            tabFocus--;
+            if (tabFocus < 0) {
+                tabFocus = tabs.length - 1;
+            }
         }
+        
+        tabs[tabFocus].setAttribute("tabindex", 0);
+        tabs[tabFocus].focus();
     }
-
-    if (e.keyCode === keydownLeft) {
-        tabFocus--
-        if (tabFocus < 0) {
-            tabFocus = tabs.length - 1
-        }
-    }
-
-    tabs[tabFocus].setAttribute('tabindex', 0)
-    tabs[tabFocus].focus()
 }
 
 function changeTabPanel(e) {
     const targetTab = e.target
     const targetPanel = targetTab.getAttribute('aria-controls')
+    const targetImage = targetTab.getAttribute('data-image')
 
     const tabContainer = targetTab.parentNode
     const mainContainer = tabContainer.parentNode
 
-    mainContainer
-        .querySelectorAll('[role="tabpanel"]')
-        .forEach(panel => {panel.setAttribute('hidden', true)})
+    tabContainer
+        .querySelector('[aria-selected="true"]')
+        .removeAttribute('aria-selected')
 
-    mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden')
+    targetTab.setAttribute('aria-selected', true)
 
+    hideContent(mainContainer, '[role="tabpanel"]')
+    showContent(mainContainer, [`#${targetPanel}`])
 
-    console.log(mainContainer)
+    hideContent(mainContainer, 'picture')
+    showContent(mainContainer, [`#${targetImage}`])
+
+}
+
+function hideContent(parent, content) {
+    parent
+        .querySelectorAll(content)
+        .forEach(item => {item.setAttribute('hidden', true)})
+}
+
+function showContent(parent, content) {
+    parent.querySelector(content).removeAttribute('hidden')
 }
